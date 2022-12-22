05808a94bfc0150ea30f570df17f69ec74409d47
ASV <aassvicente@hotmail.com>
2022-12-22 21:03:52 +0100

    mise à jour 22/12

--- front/js/cart.js
+++ front/js/cart.js
@@ -12,36 +12,37 @@
  * 4 Commande avec le formulaire 
  *   function controlFormulaire 
 */ 
-
+/**
+ * 
+ * @returns 
+ */
 function getPanierLS(){
   let panier = localStorage.getItem("panier");
   //const test133 = console.table('panier function getLs '+ panier);
   if (panier == null){
     return[];
   } else{
-    return tab = JSON.parse(panier);
+    return tableauPanier = JSON.parse(panier);
   }
 }
 
 panier = getPanierLS();
-savePanierLS(panier);
 
 
-//lecture pannier en page pannier
-/* let tabPanier= localStorage.getItem("panier");
-if (tabPanier == null){
-  panier = [];
-} 
-let panier = JSON.parse(tabPanier);*/
+//let objTest1 = localStorage.getItem("obj");
+//let test1 = JSON.parse(objTest1);
 
+/**
+* 
+* @param {*} product 
+* @param {*} product_panier 
+*
+* insertion elements html dans le DOM avec javascript
+*/
 function afficheProductAPILS(product,product_panier){
-
-  // insertion elements html dans le DOM avec javascript 
-
-  // cherche la section pointe le curseur vers id= cart__items
-  const myId = document.getElementById('cart__items'); 
   
-  // creation article ///
+  const myId = document.getElementById('cart__items'); 
+  // creation article //
   const myArticle = document.createElement("article");
   myArticle.classList.add('cart__item');
   myArticle.dataset.id= product._id;           
@@ -66,10 +67,9 @@ function afficheProductAPILS(product,product_panier){
   myDiv2.classList.add('cart__item__content');
   // creation du lien sur article
   myArticle.appendChild(myDiv2);
-  // creation div class="cart__item__content__description"
+  // creation div 
   const myDiv3 = document.createElement("div");
   myDiv3.classList.add('cart__item__content__description');
-  // creation du lien sur article
   myDiv2.appendChild(myDiv3);
   // creation h2 nom du produit
   const myH2 = document.createElement("h2");
@@ -83,36 +83,30 @@ function afficheProductAPILS(product,product_panier){
   myDiv3.appendChild(myP1);
   // creation p prix du produit
   const myP2 = document.createElement("p");
-  //myP2 = parseFloat(string)
-
   myP2.innerHTML = product.price+ '€';
   myDiv3.appendChild(myP2);
-  // creation div class="cart__item__content__settings"
+  // creation div 
   const myDiv4 = document.createElement("div");
   myDiv4.classList.add('cart__item__content__settings');
-  // creation du lien sur article
   myDiv2.appendChild(myDiv4);
-  // creation div class="cart__item__content__settings__quantity"
+  // creation div 
   const myDiv5 = document.createElement("div");
   myDiv5.classList.add('cart__item__content__settings__quantity');
-  // creation du lien sur article
   myDiv4.appendChild(myDiv5);
   // creation p quantité
   const myQuantite = document.createElement("p");
   myQuantite.innerText = 'Qté: ';                    
   myDiv5.appendChild(myQuantite);
   // creation input quantité
-  const myInput= document.createElement("input");
-  //myQuantitéInput atributs
+  const myInput= document.createElement("input");                                //myQuantitéInput atributs
   myInput.type = "number";
-  myInput.class = "itemQuantity";
+  myInput.classList.add('itemQuantity');
   myInput.name = "itemQuantity";
   myInput.min = "1";
   myInput.max = "100";
   myInput.value = product_panier.quantiteProduitPanier;                           // panier.quantiteProduitPanier
   myDiv5.appendChild(myInput);
-
-  // creation div class="cart__item__content__settings__delete"
+  // creation div 
   const myDiv6 = document.createElement("div");
   myDiv6.classList.add('cart__item__content__settings__delete');
   // creation p quantité à supprimer 
@@ -120,93 +114,87 @@ function afficheProductAPILS(product,product_panier){
   myDeleteQuantite.classList.add('deleteItem');
   myDiv6.appendChild(myDeleteQuantite);
   myDeleteQuantite.innerText= "Supprimer";
-  // creation du lien sur article
   myDiv4.appendChild(myDiv6);
-
-  // fin article 
-
-  // Affichage nombre de produits
-  //number= myInput.value;
-  //const tesTotal1 = console.table('afficher le nombre produits  produits ='+ number);
-  // inserer le total dans le DOM
-  const totalQuantity = document.getElementById('totalQuantity');
-  numberProduit = getNumberProduct();
-  const getnumber = console.table('apres function get number produits  total produits ='+ numberProduit);
-  totalQuantity.replaceChildren(numberProduit);
+  // fin creation article //
+ 
   
-  // Affiche le prix total pour le nombre de produits
-  //const price = console.table('apres function get number produits  total produits ='+ product.price);
-  let price = product.price;
-  const price2 = console.log( ' avant getTotalPrice function price =' + product.price);
-  const totalPrix = document.getElementById('totalPrice');
-  total  = getTotalPrice(price);
-  totalPrix.replaceChildren(total);
-  //const tesTotalget = console.table('apres function get number produits  total produits ='+total);
+ 
 
+}
 
-  // cibler le bon produit à suprimer/modifier avec elementClosest et dataset id et color sur id=cart__item
+/**
+ * 
+ * @param {*} product 
+ * @param {*} product_panier 
+ * 
+ * Function change quantite 
+ * Cibler le bon produit à suprimer/modifier avec elementClosest et dataset et color sur class="cart__item"
+ */
+function checkQuantity(){
   //const el = document.getElementByClassName('itemQuantity');
-  //const rarticle = el.closest('article > div');
-
+  //const closestArticle = el.closest('article > div');
   
-  // Si changement quantite  controle la quantite du input  - quantite input != quantite dans le panier 
+  //const el = document.querySelectorAll(".itemQuantity");
+  //const elementClosest = console.log(el.closest(".cart__item"));
+  
 
-  let input =  myInput.value;
-  const iquantite = console.log(" quantite" + input);
+  const formQuantity = document.querySelectorAll(".itemQuantity");
+  const noe = console.log(" function ChangeQuantity formQuantity =  "+ formQuantity);
+  
 
+  // faire la boucle indexer element.addEvent
   addEventListener('change',(event) =>{
-    myInput.value = event.target.value;
-    quantity =  myInput.value; 
-    const iquan = console.log(" quantite" + myInput.value);
-    const iqua = console.log(" quantite" + quantity);
-    if (myInput == ""){
-      input =product_panier.quantiteProduitPanier ;
-      const col1 = console.log("pas de changement de quantite " +input);
-    }else{
-      numberProduit=  changeQuantity(product,product_panier,quantity);
-  
-      // inserer le total dans le DOM
-      const totalProduit = document.getElementById('totalQuantity');
-      numberProduit = getNumberProduct();
-     // const tesTotalet = console.table('apres function get number produits  total produits ='+ numberProduit);
-      totalProduit.replaceChildren(numberProduit);
-      //totalProduit.append(numberProduit);
-
-
-      // Affiche le prix total pour le nombre de produits
-      //const price = console.table('apres function get number produits  total produits ='+ product.price);
-      const totalPrix = document.getElementById('totalPrice');
-      total = getTotalPrice(price);
-      //const pricetotal = console.table('apres function get number produits  total produits ='+total);
-      totalPrix.replaceChildren(total);
-      //const tesTotalget = console.table('apres function get number produits  total produits ='+total);
-
-    }
-  })
+    nouvelleQuantity =  event.target.value;
+    const nouqnQT = console.log(" function ChangeQuantity nouvelle quantite =  " + nouvelleQuantity);
+    ancienneQuantity = canape.quantiteProduitPanier ;
+    const ancQT = console.log(" function ChangeQuantity ancienneQuantity =  " + ancienneQuantity);
+    if (nouvelleQuantity != ancienneQuantity){
+      changeQuantity(product,nouvelleQuantity);
+     // window.location.reload();
     
-  // Button Supprime produit 
-  // Si button cliqué - produit supprimé localstorage et DOM  supprime produit du panier -
-  const suprim = document.querySelector('.deleteItem');
-  suprim.addEventListener('click', event => {
-  //const testidsup = console.log('produit à supprimer '+product._id);
-  removeProductPanier(product);
-  //const tesqt = console.table('apres function remove produit en localstorage quantity ='+quantity);
-  //let myTotal = myTotal - quantity;
-  //const tesTotal = console.table('apres function remove produits total produits ='+ myTotal);
-  });
+    }
+  })  
+}
 
-}  /// fin function afficheProductAPLILS
+function checkDelete(product){
+   /**
+  * function suprime 
+  * Button Supprime produit 
+  * Si button cliqué - produit supprimé localstorage et DOM  supprime produit du panier - 
+  */
+   
+   const suprim = document.querySelector('.deleteItem');
+   suprim.addEventListener('click', event => {
+       //const testidsup = console.log('produit à supprimer '+product._id);
+       removeProductPanier(product);
+       window.location.reload();
+       
+       //const tesqt = console.table('apres function remove produit en localstorage quantity ='+quantity);
+       //let myTotal = myTotal - quantity;
+       //const tesTotal = console.table('apres function remove produits total produits ='+ myTotal);
+   });
 
-// boucle sur le tabelau panier pour afficher les produits 
+}
+
+/**
+ * Affiche le nombre total de produits(articles) et prix total dans le DOM
+ */
+function affichetotal(){
+  const tQ = document.getElementById('totalQuantity');
+  tQ.replaceChildren(totalquantity);
+  const totalPrix = document.getElementById('totalPrice');
+  totalPrix.replaceChildren(totalprice);
+}
+
+/**
+ *  Boucle sur le tableau panier pour afficher les produits avec id localstorage
+ * 
+ */
+let totalprice = 0;
+let totalquantity = 0;
 for (var i = 0; i < panier.length; i++) {
   const canape = panier[i];
-  //  const testid = console.log('localStorage'+ canape.idProduitPanier);
-  //  const testqt = console.log('localStorage'+ canape.quantiteProduitPanier);
-  //  const testco = console.log('localStorage'+ canape.colorProduitPanier);
-
-
-  // * 2 Fetch avec id produit et affichage elements sur la page
-  let urlKanap = "http://localhost:3000/api/products/" +  canape.idProduitPanier;  // objJsonKanap.idProduitPanier
+  let urlKanap = "http://localhost:3000/api/products/" +  canape.idProduitPanier;  
  
   fetch(urlKanap)
   .then(function(res) {
@@ -216,51 +204,69 @@ for (var i = 0; i < panier.length; i++) {
   })
   .then((data) =>{
     const test5 = console.table(data);
-    afficheProductAPILS(data,canape);           // data API canape dans le Localstorage
-          
+    afficheProductAPILS(data,canape);           // data API et canapes dans le Localstorage
+    
+    checkDelete(canape)
+    totalprice = totalprice + Number(data.price) * Number(canape.quantiteProduitPanier);
+    totalquantity = totalquantity + Number(canape.quantiteProduitPanier);
+    affichetotal()
+   
   });
-
+  checkQuantity();
 }  // fin de boucle for sur tableau objects product panier 
+
 
 /**
  * 3. Gestion Pannier 
  *    Ecoute button supprime quantite ou delete produit
  *    Functions
- *    saveKanapLocalstorage - getKanapLocastorage - addKanapPanier,  removeKanapPannier, changeQuantity, 
- * 
+ *    savePanierLS -   removeProductPanier, changeQuantity, 
 */ 
- 
+
+/**
+ * 
+ * @param {*} panier 
+ */
 function savePanierLS(panier){
   localStorage.setItem("panier",JSON.stringify(panier));
   if (panier == undefined){
     panier = [];
   }
-
 }
-
+/**
+ * 
+ * @param {*} product 
+ * 
+ * sauvegarde le panier en excluant le produit passé en parametre  
+ */
 function removeProductPanier(product){
   let panier = getPanierLS();
- // const test173 = console.table('remove function  '+ panier[0].idProduitPanier);
-  idProduit = panier[0].idProduitPanier;
- // const test133 = console.table('remove function  '+ product._id);
+  // const test133 = console.table('remove function  '+ product._id);
   panier = panier.filter(p => p.idProduit != product._id);
-  if (panier == undefined){
-  //  const test143 = console.table('remove function panier == undefined ');
-    panier = [];
-    localStorage.removeItem(panier[0].idProduitPanier);
-  }
+  savePanierLS(panier);
 }
-
-function changeQuantity(product,product_panier,quantity){
+/**
+ * 
+ * @param {*} product 
+ * @param {*} product_panier 
+ * @param {*} nouvelleQuantity 
+ */
+function changeQuantity(product,nouvelleQuantity){
   let panier = getPanierLS();
   let foundProduct = panier.find(p =>p.idProduitPanier == product._id && p.colorProduitPanier== product_panier.colorProduitPanier);
   if (foundProduct != undefined){
-    foundProduct.quantiteProduitPanier = quantity;
-    
+    foundProduct.quantiteProduitPanier = nouvelleQuantity;
+    if(foundProduct.quantiteProduitPanier<=0){
+      removeProductPanier(foundProduct);
+    }else{
+      savePanierLS(panier);
+    }
     
   }
-  savePanierLS(panier);
+ 
 }
+
+
 /*
 function changeQuantity(product,quantity){
 
@@ -273,27 +279,10 @@ function changeQuantity(product,quantity){
   return quantity;
 }
 */
-function getNumberProduct(){
-  let panier = getPanierLS();
-  let number = 0;
-  for (let product of panier){
-    number = number + Number(product.quantiteProduitPanier) ;
-  }
-  return number;
-}
 
-function getTotalPrice(price){
-  let panier =getPanierLS();
-  let total = 0;
-  for (let product of panier) {
-    let quantite = Number(product.quantiteProduitPanier);
-    let prix= Number(price);
-    total = quantite * prix;
-    const pri = console.log( ' getTotalPrice function prix =' + prix);
-    const qunat = console.log( ' getTotalPrice function  quantite = ' +  product.quantiteProduitPanier);
-    const totalPrix = console.log( ' getTotalPrice function  total = ' +  total);
-  }
-  return total;
-}
+
+//  const testid = console.log('localStorage'+ canape.idProduitPanier);
+  //  const testqt = console.log('localStorage'+ canape.quantiteProduitPanier);
+  //  const testco = console.log('localStorage'+ canape.colorProduitPanier);
 
 
